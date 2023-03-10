8f513dc1ffbe14f66b6b6f242a798b92961f2613
gtengine <prierkt@gmail.com>
2023-03-10 20:56:19 +0900

    justify, align

--- front-end/src/view/station/sign/signIn.tsx
+++ front-end/src/view/station/sign/signIn.tsx
@@ -1,15 +1,12 @@
 import { NextPage } from "next";
 import { NextRouter, useRouter } from "next/router";
-import "@/src/view/layout/layout.module.css";
 
 const SignInCom: NextPage = () => {
   const router: NextRouter = useRouter();
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div>
-        <h1>로그인</h1>
-        <p>환영합니다.</p>
-      </div>
+    <div className="flex flex-col space-y-80 justify-between items-center h-screen">
+      <div className="">hello</div>
+      <div>world</div>
     </div>
   );
 };
